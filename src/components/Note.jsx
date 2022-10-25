7828589e03211b96b3fbca212086c9bcefc3a3de
Punith <punithkcaditya@gmail.com>
2022-10-25 11:05:26 +0530

    Update Note.jsx

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,16 +1,48 @@
-import React from 'react'
+import {useState,React} from 'react'
 
 const Note = ({note,deleteNote}) => {
+
+const [modeEdit, setModeEdit] = useState(false);
+const [item, setItem] = useState(note);
+
+const toggle = () => {
+  setModeEdit(!modeEdit);
+  setItem(note)
+}
+
   return (
   
       
         <li style={{marginBottom: '.6rem'}}>
-          id : {note.id}, title: {note.title}, body: {note.body}
-          <button onClick={()=> deleteNote(note.id)}>X</button>
+          <div>id : {note.id}</div>
+          {
+            modeEdit ? 
+            <label>
+              <input type="text" value={item.title}  onChange={(ev) => setItem({...item, title: ev.target.value})}/>
+            </label>:<div>title: {note.title}</div>
+
+          }
+          {
+            modeEdit ? 
+            <label>
+              <input type="text" value={item.body} onChange={(ev) => setItem({...item, body: ev.target.value})}/>
+            </label>:<div>body: {note.body}</div>
+
+          }
+           <button onClick={()=> toggle()}>{modeEdit ? 'cortar' : 'editar'}</button>
+           {
+                 modeEdit &&
+                 <button>nuero</button>
+           }
+           {
+            !modeEdit &&
+            <button onClick={()=> deleteNote(note.id)}>X</button>
+
+           }
         </li>
       
    
-  )
+  );
 }
 
 export default Note;
